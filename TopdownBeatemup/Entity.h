470d0136def3523b7551c20ad9d4eac8b334805e
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-04-01 04:24:16 +0100

    Create and setup abstract Entity class

---
+++ TopdownBeatemup/Entity.h
@@ -0,0 +1,21 @@
+#ifndef _ENTITY_H_
+#define _ENTITY_H_
+
+class Entity
+{
+// Shared variables
+protected:
+	// Entity coordinates
+	float mX{ 0 }, mY{ 0 };
+
+// Pure virtual methods
+public:
+	// Update method
+	void virtual Update() = 0;
+	// Render method
+	void virtual Render() = 0;
+
+	// Default CTOR & DTOR
+};
+
+#endif

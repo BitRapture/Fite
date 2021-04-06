e256596f94f51be211f387fd6f7ef8a06253b212
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-04-06 01:59:15 +0100

    Add methods/members to enemy, test collision with player

--- TopdownBeatemup/Enemy.cpp
+++ TopdownBeatemup/Enemy.cpp
@@ -1,1 +1,26 @@
 #include "Enemy.h"
+
+void Enemy::Update(double& _deltaTime)
+{
+	if (!mFocus->CheckCollision(*this)) mY += 0.5f * _deltaTime;
+	if (mY > 784) mY = -64;
+
+	float rad = atan2f(mY - mFocus->GetY(), mX - mFocus->GetX());
+	mBaseSprite.SetSpriteAngle((rad >= 0 ? rad : (2 * 3.1415f) + rad) * (180 / 3.1415f));
+}
+
+void Enemy::Render()
+{
+	mBaseSprite.RenderSprite(mContext, mX, mY);
+}
+
+Enemy::Enemy(SDL_Renderer* _ctx, SDL_Texture* _enemySprite, Player* _player)
+:
+	Character(_ctx, _enemySprite, {0, 0, 64, 64}, 1, 1, 20),
+	mFocus{ _player }
+{
+	mBaseSprite.mSpriteXOffset = -32;
+	mBaseSprite.mSpriteYOffset = -32;
+	mX = 400;
+	mY = 400;
+}
--- TopdownBeatemup/Enemy.h
+++ TopdownBeatemup/Enemy.h
@@ -2,10 +2,24 @@
 #define _ENEMY_H_
 
 #include "Character.h"
+#include "Player.h"
 
 class Enemy : public Character
 {
+// Hidden variables
+private:
+	// Pointer to player
+	Player* mFocus{ nullptr };
 
+
+// Shared methods
+public:
+	// Entity method overrides
+	void Update(double& _deltaTime) override;
+	void Render() override;
+
+	// Constructor
+	Enemy(SDL_Renderer* _ctx, SDL_Texture* _enemySprite, Player* _player);
 };
 
 #endif

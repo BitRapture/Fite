c982f9846599a8374b751b9950b4cfb1e6dfdb3c
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-04-08 03:27:14 +0100

    Add related methods and members

--- TopdownBeatemup/GameObjectManager.h
+++ TopdownBeatemup/GameObjectManager.h
@@ -8,14 +8,41 @@ class GameObjectManager
 {
 // Hidden variables
 private:
-	// List of all objects 
-	std::vector<GameObject*> mObjects;
+	// Cell dimensions
+	float mCellWidth{ 0 }, mCellHeight{ 0 };
+	// Spatial partitioning grid
+	std::vector<GameObject*>* mGrid{ nullptr };
+	// Grid size
+	int mGridSize{ 0 };
+
+// GameObjectManager flags
+	// Grid initialised
+	bool mGridInitialised{ false };
 
 // Public methods
 public:
-	// Add object to the system
-	bool AddObject(GameObject* _object);
+	// Check collisions for GameObject
+	void CheckCollisions(GameObject* _object);
 
+	// Add GameObject ref to grid
+	void AddObject(GameObject* _object, int _index);
+	// Remove GameObject ref from grid
+	void RemoveObject(GameObject* _object, int _index);
+
+	// Get cell dimensions
+	float GetCellWidth();
+	float GetCellHeight();
+
+	// Delete and clean up manager
+	void Delete();
+
+	// Check manager is initialised
+	bool IsInit();
+
+	// Constructor
+	GameObjectManager(int _gameSpaceW, int _gameSpaceH, int _cellAmount);
+	// Destructor
+	~GameObjectManager();
 };
 
 #endif

eb1954b1de68b69d50b1e7907904c5ee48f10781
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-05-13 03:11:19 +0100

    Test collision manager and Bullet class

--- TopdownBeatemup/Enemy.cpp
+++ TopdownBeatemup/Enemy.cpp
@@ -2,9 +2,6 @@
 
 void Enemy::Update(double& _deltaTime)
 {
-	if (!mFocus->CheckCollision(*this)) mY += 0.5f * _deltaTime;
-	if (mY > 784) mY = -64;
-
 	float rad = atan2f(mY - mFocus->GetY(), mX - mFocus->GetX());
 	mBaseSprite.SetSpriteAngle((rad >= 0 ? rad : (2 * 3.1415f) + rad) * (180 / 3.1415f));
 }
@@ -16,11 +13,12 @@ void Enemy::Render()
 
 Enemy::Enemy(SDL_Renderer* _ctx, SDL_Texture* _enemySprite, Player* _player, float _startingX, float _startingY)
 :
-	Character(_ctx, _enemySprite, {0, 0, 64, 64}, 1, 1, 20, _startingX, _startingY),
+	Character(_ctx, _enemySprite, {0, 0, 64, 64}, 1, 1, 16, _startingX, _startingY),
 	mFocus{ _player }
 {
 	mBaseSprite.mSpriteXOffset = -32;
 	mBaseSprite.mSpriteYOffset = -32;
 	mX = _startingX;
 	mY = _startingY;
+	mCollidable = true;
 }

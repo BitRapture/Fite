ed619668293da60730de87b5bc4d4ac3eabb475f
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-05-13 03:10:13 +0100

    Remove useless direction methods, add getters for direction vector

--- TopdownBeatemup/GameObject.cpp
+++ TopdownBeatemup/GameObject.cpp
@@ -7,45 +7,19 @@ void GameObject::SetDirection(float _angle)
 	mDirY = (tempX * sin) + (mDirY * cos);
 }
 
-void GameObject::SetCardinalDir(int _dir)
+bool GameObject::PredictCollision(GameObject* _object, float& _x, float& _y, float& _searchRadius)
 {
-	switch (_dir)
-	{
-	// Facing up
-	case 0: { mDirX = 0; mDirY = -1; break; }
-	// Facing up-right
-	case 1: { mDirX = 0.7071067811865476f; mDirY = -0.707106781186547f; break; }
-	// Facing right
-	case 2: { mDirX = 1; mDirY = 0; break; }
-	// Facing down-right
-	case 3: { mDirX = 0.7071067811865475f; mDirY = 0.707106781186547f; break; }
-	// Facing down
-	case 4: { mDirX = 0; mDirY = 1; break; }
-	// Facing down-left
-	case 5: { mDirX = -0.7071067811865477f; mDirY = 0.7071067811865475f; break; }
-	// Facing left
-	case 6: { mDirX = -1; mDirY = 0; break; }
-	// Facing up-left
-	case 7: { mDirX = -0.7071067811865474f; mDirY = -0.7071067811865477f; break; }
-	}
+	return ((powf(_x - _object->mX, 2) + powf(_y - _object->mY, 2)) <= powf(_searchRadius + _object->mSize, 2));
 }
 
-bool GameObject::CheckCollision(GameObject* _object)
+float GameObject::GetDirX()
 {
-	return ((powf(mX - _object->mX, 2) + powf(mY - _object->mY, 2)) <= powf(mSize + _object->mSize, 2));
+	return mDirX;
 }
 
-bool GameObject::CheckAABBCollision(GameObject* _object)
+float GameObject::GetDirY()
 {
-	SDL_Rect aSize = this->mBaseSprite.GetSpriteSize(), bSize = _object->mBaseSprite.GetSpriteSize();
-	float aX = mX + mBaseSprite.mSpriteXOffset, aY = mY + mBaseSprite.mSpriteYOffset,
-		bX = _object->mX + _object->mBaseSprite.mSpriteXOffset, bY = _object->mX + _object->mBaseSprite.mSpriteXOffset;
-	return (
-		aX < bX + (bSize.w + _object->mBaseSprite.mSpriteXOffset) &&
-		aY < bY + (bSize.h + _object->mBaseSprite.mSpriteYOffset) &&
-		aX + (aSize.w + mBaseSprite.mSpriteXOffset) > bX &&
-		aY + (aSize.h + mBaseSprite.mSpriteYOffset) > bY
-		);
+	return mDirY;
 }
 
 GameObject::GameObject(SDL_Renderer* _ctx, SDL_Texture* _sprite, SDL_Rect& _spriteSize, int& _maxXFrames, int& _maxYFrames, float& _objectSize, float& _startingX, float& _startingY)
--- TopdownBeatemup/GameObject.h
+++ TopdownBeatemup/GameObject.h
@@ -5,19 +5,11 @@
 #include <vector>
 #include "Sprite.h"
 #include "Entity.h"
-#include "GameObjectManager.h"
 
 class GameObject : public Entity
 {
-// Share private variables with the object manager
-friend class GameObjectManager;
-
-// Hidden variables
-private:
-	// Pointer to the object managing system
-	GameObjectManager* mObjectManager{ nullptr };
-	// GameObjectManager grid indices 
-	std::vector<int> mOMIndices;
+// Let the collision manager view private variables
+friend class CollisionManager;
 
 // Shared variables
 protected:
@@ -31,26 +23,32 @@ protected:
 	// GameObject direction (unit vector) 
 	// (pointing right by default)
 	float mDirX{ 1 }, mDirY{ 0 };
+
 	// GameObject collision stack
-	std::vector<GameObject*> mCollisions;
+	// Allows *this to handle other GameObject collisions
+	std::vector<GameObject*> mCollisionStack;
+
+// GameObject flags
+	// Does this object collide with other objects
+	bool mCollidable{ false };
 
 // Shared methods
 protected:
 	// Set direction (radians)
 	void SetDirection(float _angle);
-	// Set cardinal direction (0 to 7) (clockwise)
-	// Precalculated unit vector directions
-	void SetCardinalDir(int _dir);
 
 // Public methods
 public:
-	// Circle-Circle collision (main form of object collision)
-	bool CheckCollision(GameObject* _object);
-	// AABB collision, based off sprite size
-	bool CheckAABBCollision(GameObject* _object);
+	// Predict Circle-Circle collision, used to precalculate movement, attacks, etc
+	bool PredictCollision(GameObject* _object, float& _x, float& _y, float& _searchRadius);
+
+	// Get direction X
+	float GetDirX();
+	// Get direction Y
+	float GetDirY();
 
 public:
-	// Constructors
+	// Constructor
 	GameObject(SDL_Renderer* _ctx, SDL_Texture* _sprite, SDL_Rect& _spriteSize, int& _maxXFrames, int& _maxYFrames, float& _objectSize, float& _startingX, float& _startingY);
 	// Destructor
 	~GameObject();
@@ -64,8 +62,5 @@ public:
 // Let mX mY get offset by camera pos
 // Add mCollided pointer
 //		-> Lets objects sort out collisions with one another
-// Move mContext to Entity.h
-//		-> All entities should have a pointer to the renderer
-//		-> overide the default entity ctor
 
 #endif

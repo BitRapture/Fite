9fca035a2c270c1909967f0243ea83f32b29aaf7
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-05-15 00:17:00 +0100

    Add audio and text fonts extensions

--- TopdownBeatemup/main.cpp
+++ TopdownBeatemup/main.cpp
@@ -2,8 +2,10 @@
 
 int main(int argc, char** argv)
 {
-	if (SDL_Init(SDL_INIT_EVERYTHING) != 0 ) return 400;
+	if (SDL_Init(SDL_INIT_VIDEO) != 0 ) return 400;
 	if (IMG_Init(IMG_INIT_PNG) == 0) return 401;
+	if (Mix_Init(MIX_INIT_MP3) == 0) return 402;
+	if (TTF_Init() == -1) return 403;
 
 	GameManager Game;
 

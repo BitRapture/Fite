aa5045f407e77d15152e2ce94a0804066e832bda
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-05-15 00:20:30 +0100

    Add internal methods, rework for resource management

--- TopdownBeatemup/GameObject.cpp
+++ TopdownBeatemup/GameObject.cpp
@@ -7,6 +7,16 @@ void GameObject::SetDirection(float _angle)
 	mDirY = (tempX * sin) + (mDirY * cos);
 }
 
+void GameObject::DrawHitboxes()
+{
+	// Set color to yellow
+	SDL_SetRenderDrawColor(mContext, 255, 255, 0, 255);
+	// Draw a box (actually the circle radius, easier to display as a rect though)
+	SDL_Rect test{ mX - mSize, mY - mSize, mSize * 2, mSize * 2 };
+	// Render hitbox around object
+	SDL_RenderDrawRect(mContext, &test);
+}
+
 bool GameObject::PredictCollision(GameObject* _object, float& _x, float& _y, float& _searchRadius)
 {
 	return ((powf(_x - _object->mX, 2) + powf(_y - _object->mY, 2)) <= powf(_searchRadius + _object->mSize, 2));
@@ -22,16 +32,60 @@ float GameObject::GetDirY()
 	return mDirY;
 }
 
-GameObject::GameObject(SDL_Renderer* _ctx, SDL_Texture* _sprite, SDL_Rect& _spriteSize, int& _maxXFrames, int& _maxYFrames, float& _objectSize, float& _startingX, float& _startingY)
+int GameObject::GetID()
+{
+	return mID;
+}
+
+GameObject* GameObject::AccessCreationQueue(size_t _index)
+{
+	if (_index >= mCreationQueue.size()) return nullptr;
+	return mCreationQueue[_index];
+}
+
+bool GameObject::AccessCreationQueueCollisions(size_t _index)
+{
+	if (_index >= mCreationQueue.size()) return false;
+	return mCreationCollidable[_index];
+}
+
+size_t GameObject::GetCreationQueueSize()
+{
+	return mCreationQueue.size();
+}
+
+void GameObject::CreateObject(GameObject* _object, bool _selfCollidable)
+{
+	mCreationQueue.push_back(_object);
+	mCreationCollidable.push_back(_selfCollidable);
+}
+
+void GameObject::ClearCreationQueue()
+{
+	mCreationQueue.clear();
+	mCreationCollidable.clear();
+}
+
+GameObject::GameObject(SDL_Renderer* _ctx, ResourceManager* _resources, SDL_Rect& _spriteSize, int& _ID, int& _maxXFrames, int& _maxYFrames, float& _objectSize, float& _startingX, float& _startingY)
 	:
 	Entity(_ctx, _startingX, _startingY),
-	mBaseSprite(_sprite, _spriteSize, _maxXFrames, _maxYFrames),
-	mSize{ _objectSize }
+	mResources{ _resources },
+	mBaseSprite(_resources->GetSpriteSheet(_ID), _spriteSize, _maxXFrames, _maxYFrames),
+	mSize{ _objectSize }, mSearchSize{ _objectSize }, mID{ _ID }
 {
 
 }
 
 GameObject::~GameObject()
 {
-
+	// Destroy any objects in creation queue
+	for (std::vector<GameObject*>::iterator it = mCreationQueue.begin();
+		it != mCreationQueue.end(); ++it)
+	{
+		// Delete object on heap
+		delete (*it);
+		// remove dangling pointers
+		(*it) = nullptr;
+	}
+	mCollisionStack.clear();
 }
--- TopdownBeatemup/GameObject.h
+++ TopdownBeatemup/GameObject.h
@@ -5,11 +5,20 @@
 #include <vector>
 #include "Sprite.h"
 #include "Entity.h"
+#include "GameObjects.h"
+#include "ResourceManager.h"
 
 class GameObject : public Entity
 {
 // Let the collision manager view private variables
 friend class CollisionManager;
+
+// Hidden variables
+private:
+	// Related to object creation
+	std::vector<GameObject*> mCreationQueue;
+	// Array of bools to check if new object at index is collidable with itself
+	std::vector<bool> mCreationCollidable;
 
 // Shared variables
 protected:
@@ -20,13 +29,21 @@ protected:
 // Generic GameObject variables
 	// GameObject size (circle radius)
 	float mSize{ 0 };
+	// GameObject collision search size (circle radius)
+	float mSearchSize{ 0 };
 	// GameObject direction (unit vector) 
 	// (pointing right by default)
 	float mDirX{ 1 }, mDirY{ 0 };
+	// GameObject generic ID, used for other GameObjects to identify its
+	// specific extended class
+	int mID{ 0 };
 
 	// GameObject collision stack
 	// Allows *this to handle other GameObject collisions
 	std::vector<GameObject*> mCollisionStack;
+
+	// Link to resource manager for media access
+	ResourceManager* mResources;
 
 // GameObject flags
 	// Does this object collide with other objects
@@ -36,6 +53,9 @@ protected:
 protected:
 	// Set direction (radians)
 	void SetDirection(float _angle);
+
+	// Debug draw hitboxes
+	void DrawHitboxes();
 
 // Public methods
 public:
@@ -47,9 +67,23 @@ public:
 	// Get direction Y
 	float GetDirY();
 
+	// Get ID
+	int GetID();
+
+	// Access creation queue
+	GameObject* AccessCreationQueue(size_t _index);
+	// Access creation collisions
+	bool AccessCreationQueueCollisions(size_t index);
+	// Check size of creation queue
+	size_t GetCreationQueueSize();
+	// Add new object to creation queue
+	void CreateObject(GameObject* _object, bool _selfCollidable = false);
+	// Clear creation queue
+	void ClearCreationQueue();
+
 public:
 	// Constructor
-	GameObject(SDL_Renderer* _ctx, SDL_Texture* _sprite, SDL_Rect& _spriteSize, int& _maxXFrames, int& _maxYFrames, float& _objectSize, float& _startingX, float& _startingY);
+	GameObject(SDL_Renderer* _ctx, ResourceManager* _resources, SDL_Rect& _spriteSize, int& _ID, int& _maxXFrames, int& _maxYFrames, float& _objectSize, float& _startingX, float& _startingY);
 	// Destructor
 	~GameObject();
 

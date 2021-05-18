00ce1ea0658627197573050be90b7258cd13a835
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-05-18 21:34:21 +0100

    Initialise audio system, dynamically instantiate game

--- TopdownBeatemup/main.cpp
+++ TopdownBeatemup/main.cpp
@@ -2,14 +2,20 @@
 
 int main(int argc, char** argv)
 {
-	if (SDL_Init(SDL_INIT_VIDEO) != 0 ) return 400;
+	// Initialise all SDL extensions
+	if (SDL_Init(SDL_INIT_VIDEO | SDL_INIT_AUDIO) != 0 ) return 400;
 	if (IMG_Init(IMG_INIT_PNG) == 0) return 401;
-	if (Mix_Init(MIX_INIT_MP3) == 0) return 402;
+	if (Mix_OpenAudio(22050, AUDIO_S16SYS, 2, 4096) == -1) return 402;
 	if (TTF_Init() == -1) return 403;
 
-	GameManager Game;
+	// Creating this dynamically enables us specifiy when exactly to destroy it
+	GameManager* Game = new GameManager;
 
-	Game.Run();
+	// Run the game
+	Game->Run();
+
+	// Delete the game, must be done this way as SDL extensions need to quit before SDL_Quit
+	delete Game;
 
 	SDL_Quit();
 	return 200;

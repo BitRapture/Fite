2e3150d548ebfe323b35028b324fdc00efe7090a
BitRapture <thinzy.business+bitrapture@gmail.com>
2021-05-18 21:28:15 +0100

    Alter base class

--- TopdownBeatemup/Character.cpp
+++ TopdownBeatemup/Character.cpp
@@ -8,6 +8,11 @@ void Character::CheckHealth()
 	}
 }
 
+int Character::GetHealth()
+{
+	return mHealth;
+}
+
 Character::Character(SDL_Renderer* _ctx, ResourceManager* _resources, SDL_Rect _spriteSize, int _ID, int _maxXFrames, int _maxYFrames, float _objectSize, float _startingX, float _startingY)
 :
 	GameObject(_ctx, _resources, _spriteSize, _ID, _maxXFrames, _maxYFrames, _objectSize, _startingX, _startingY),
--- TopdownBeatemup/Character.h
+++ TopdownBeatemup/Character.h
@@ -21,6 +21,9 @@ protected:
 	void CheckHealth();
 
 public:
+	// Get health of character
+	int GetHealth();
+
 	// Constructor
 	Character(SDL_Renderer* _ctx, ResourceManager* _resources, SDL_Rect _spriteSize, int _ID, int _maxXFrames, int _maxYFrames, float _objectSize, float _startingX, float _startingY);
 	// Destructor
